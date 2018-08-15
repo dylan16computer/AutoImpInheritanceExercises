c3e803e82761b268137c843c2ad42a5a5c56d025
Dylan Hodder <30002799@student.toiohomai.ac.nz>
2018-08-16 09:15:41 +1200

    Exercise completed.

---
+++ Exercises/Exercises/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Person
+    {
+        public string Fname{ get; set; }
+        public string Lname{ get; set; }
+    }
+    class Student:Person
+    {
+        public int StudentID { get; set; }
+        public string GetStdtDeets()
+        {
+            return $"\nFirst Name: {Fname}\nLast Name: {Lname}\nStudent ID: {StudentID}\n";
+        }
+        public Student(string _fname, string _lname, int _stdid)
+        {
+            Fname = _fname;
+            Lname = _lname;
+            StudentID = _stdid;
+        }
+    }
+    class Employee:Person
+    {
+        public string EmployeeID { get; set; }
+    }
+    class Teacher:Employee
+    {
+        public string Office { get; set; }
+        public string GetTeachDeets()
+        {
+            return $"\nFirst Name: {Fname}\nLast Name: {Lname}\nEmployee ID: {EmployeeID}\nOffice: {Office}\n";
+        }
+        public Teacher(string _fname, string _lname, string _empid, string _office)
+        {
+            Fname = _fname;
+            Lname = _lname;
+            EmployeeID = _empid;
+            Office = _office;
+        }
+    }
+}
---
+++ Exercises/Exercises/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student>();
+            var teachers = new List<Teacher>();
+            students.Add(new Student("John", "Smith", 1234));
+            students.Add(new Student("Kevin", "James", 4321));
+            students.Add(new Student("Adam", "Richards", 9876));
+            students.Add(new Student("Mike", "Hype", 4765));
+            students.Add(new Student("Jack", "Hill", 2947));
+            teachers.Add(new Teacher("Lily", "Sass", "5263", "Level 5"));
+            teachers.Add(new Teacher("Mandy", "Lass", "1029", "Level 10"));
+            teachers.Add(new Teacher("Emma", "Watson", "9571", "Level 7"));
+            teachers.Add(new Teacher("Jill", "Sally", "2401", "Level 2"));
+            teachers.Add(new Teacher("Hilda", "James", "5263", "Level 1"));
+            Console.WriteLine("--- Student Info ---");
+            foreach(Student x in students)
+            {
+                Console.WriteLine(x.GetStdtDeets());
+            }
+            Console.WriteLine("\n--- Teacher Info ---");
+            foreach(Teacher y in teachers)
+            {
+                Console.WriteLine(y.GetTeachDeets());
+            }
+        }
+    }
+}
